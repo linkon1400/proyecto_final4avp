f3cc2c7c65789800efcaa5b538d3fd0c7d832282
linkon1400 <johnpaulbernal6@gmail.com>
2024-07-01 02:57:36 -0700

    Add files via upload

---
+++ cafe.php
@@ -0,0 +1,9 @@
+<?php
+$nomb = $_GET['nomb'];
+$num = $_GET['num'];
+$pedi = $_GET['pedi'];
+
+echo $nomb. "<br>";
+echo $num. "<br>";
+echo $pedi. "<br>";
+ ?> <link rel="stlesheet"  href="john.css">
